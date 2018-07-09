02c7307f403c9508c4594fc8cf8b832975f17b38
Kai Su <skai@vmware.com>
2018-07-09 16:17:56 -0700

    Add initial changes related to EQ support

--- functions/create_hosts.sh
+++ functions/create_hosts.sh
@@ -1,162 +1,73 @@
 #!/bin/bash
 
 function create_controller_hosts {
-  if [ "$NSX_T_CONTROLLERS_CONFIG" == "" -o "$NSX_T_CONTROLLERS_CONFIG" == "null" ]; then
-    create_controller_hosts_on_cluster
-  else
-    create_controller_hosts_across_clusters
-  fi
-}
-
-function create_controller_hosts_on_cluster {
-
   count=1
-  echo "[nsxcontrollers]" > ctrl_vms
-  for controller_ip in $(echo $NSX_T_CONTROLLER_IPS | sed -e 's/,/ /g')
+  echo "[controllers]" > ctrl_vms
+  for controller_ip in $(echo $controller_ips_int | sed -e 's/,/ /g')
   do
     cat >> ctrl_vms <<-EOF
-nsx-controller0${count} \
-  ansible_ssh_host=$controller_ip \
-  ansible_ssh_user=root \
-  ansible_ssh_pass=$NSX_T_CONTROLLER_ROOT_PWD \
-  dc="$VCENTER_DATACENTER" \
-  cluster="$VCENTER_CLUSTER" \
-  resource_pool="$VCENTER_RP" \
-  datastore="$VCENTER_DATASTORE" \
-  portgroup="$MGMT_PORTGROUP" \
-  gw=$DEFAULTGATEWAY \
-  mask=$NETMASK \
-  vmname="${NSX_T_CONTROLLER_VM_NAME_PREFIX}-0${count}" \
-  hostname="${NSX_T_CONTROLLER_HOST_PREFIX}-0${count}"
+  controller-${count} ip=$controller_ip hostname=${controller_hostname_prefix_int}-${count} default_gateway=${controller_default_gateway_int} prefix_length=$controller_ip_prefix_length_int
 EOF
     (( count++ ))
   done
-
-}
-
-function create_controller_hosts_across_clusters {
-
-  count=1
-  echo "[nsxcontrollers]" > ctrl_vms
-
-  echo "$NSX_T_CONTROLLERS_CONFIG" > /tmp/controllers_config.yml
-  is_valid_yml=$(cat /tmp/controllers_config.yml  | shyaml get-values controllers || true)
-
-  # Check if the esxi_hosts config is not empty and is valid
-  if [ "$NSX_T_CONTROLLERS_CONFIG" != "" -a "$is_valid_yml" != "" ]; then
-
-    NSX_T_CONTROLLER_VM_NAME_PREFIX=$(cat /tmp/controllers_config.yml  | shyaml get-value controllers.vm_name_prefix)
-    NSX_T_CONTROLLER_HOST_PREFIX=$(cat /tmp/controllers_config.yml  | shyaml get-value controllers.host_prefix)
-    NSX_T_CONTROLLER_ROOT_PWD=$(cat /tmp/controllers_config.yml  | shyaml get-value controllers.root_pwd)
-    NSX_T_CONTROLLER_CLUSTER_PWD=$(cat /tmp/controllers_config.yml  | shyaml get-value controllers.cluster_pwd)
-
-    length=$(expr $(cat /tmp/controllers_config.yml  | shyaml get-values controllers.members | grep ip: | wc -l) - 1 || true )
-    if ! [ $length == 0 -o $length == 2 ]; then
-      echo "Error with # of controllers - should be odd (1 or 3)!!"
-      echo "Exiting!!"
-      exit -1
-    fi
-
-    for index in $(seq 0 $length)
-    do
-      NSX_T_CONTROLLER_INSTANCE_IP=$(cat /tmp/controllers_config.yml  | shyaml get-value controllers.members.${index}.ip)
-      NSX_T_CONTROLLER_INSTANCE_CLUSTER=$(cat /tmp/controllers_config.yml  | shyaml get-value controllers.members.${index}.cluster)
-      NSX_T_CONTROLLER_INSTANCE_RP=$(cat /tmp/controllers_config.yml  | shyaml get-value controllers.members.${index}.resource_pool)
-      NSX_T_CONTROLLER_INSTANCE_DATASTORE=$(cat /tmp/controllers_config.yml  | shyaml get-value controllers.members.${index}.datastore)
-
-      cat >> ctrl_vms <<-EOF
-      nsx-controller0${count} \
-        ansible_ssh_host=$NSX_T_CONTROLLER_INSTANCE_IP \
-        ansible_ssh_user=root \
-        ansible_ssh_pass=$NSX_T_CONTROLLER_ROOT_PWD \
-        dc="$VCENTER_DATACENTER" \
-        cluster="$NSX_T_CONTROLLER_INSTANCE_CLUSTER" \
-        resource_pool="$NSX_T_CONTROLLER_INSTANCE_RP" \
-        datastore="$NSX_T_CONTROLLER_INSTANCE_DATASTORE" \
-        portgroup="$MGMT_PORTGROUP" \
-        gw=$DEFAULTGATEWAY \
-        mask=$NETMASK \
-        vmname="${NSX_T_CONTROLLER_VM_NAME_PREFIX}-0${count}" \
-        hostname="${NSX_T_CONTROLLER_HOST_PREFIX}-0${count}"
+  cat >> ctrl_vms <<-EOF
+[controllers:vars]
+controller_cli_password="$controller_cli_password_int"
+controller_root_password="$controller_root_password_int"
+controller_deployment_size="$controller_deployment_size_int"
+controller_compute_id="$controller_compute_id_int"
+controller_storage_id="$controller_storage_id_int"
+controller_shared_secret="$controller_shared_secret_int"
 EOF
-      (( count++ ))
-    done
-fi
 
 }
 
+# TODO: update this with params from https://github.com/yasensim/nsxt-ansible/blob/master/answerfile.yml
 function create_edge_hosts {
-  if [ "$EDGE_VCENTER_HOST" != "" -a "$EDGE_VCENTER_HOST" != "null" ]; then
-    create_edge_hosts_using_edge_vcenter
-  else
-    create_edge_hosts_using_mgmt_vcenter
-  fi
-}
-
-function create_edge_hosts_using_edge_vcenter {
   count=1
-  echo "[nsxedges]" > edge_vms
-  for edge_ip in $(echo $NSX_T_EDGE_IPS | sed -e 's/,/ /g')
+  echo "[edge_nodes]" > edge_vms
+  for edge_ip in $(echo $edge_ips_int | sed -e 's/,/ /g')
   do
     cat >> edge_vms <<-EOF
-${NSX_T_EDGE_HOST_PREFIX}-0${count}  \
-  ansible_ssh_host=$edge_ip \
-  ansible_ssh_user=root \
-  ansible_ssh_pass=$NSX_T_EDGE_ROOT_PWD \
-  vcenter_host="$EDGE_VCENTER_HOST" \
-  vcenter_user="$EDGE_VCENTER_USR" \
-  vcenter_pwd="$EDGE_VCENTER_PWD" \
-  dc="$EDGE_VCENTER_DATACENTER" \
-  datastore="$EDGE_VCENTER_DATASTORE" \
-  cluster="$EDGE_VCENTER_CLUSTER" \
-  resource_pool="$EDGE_VCENTER_RP" \
-  dns_server="$EDGE_DNSSERVER" \
-  dns_domain="$EDGE_DNSDOMAIN" \
-  ntp_server="$EDGE_NTPSERVERS" \
-  gw="$EDGE_DEFAULTGATEWAY" \
-  mask="$EDGE_NETMASK" \
-  vmname="${NSX_T_EDGE_VM_NAME_PREFIX}-0${count}" \
-  hostname="${NSX_T_EDGE_HOST_PREFIX}-0${count}" \
-  portgroup="$EDGE_MGMT_PORTGROUP" \
-  portgroupExt="$NSX_T_EDGE_PORTGROUP_EXT" \
-  portgroupTransport="$NSX_T_EDGE_PORTGROUP_TRANSPORT"
-EOF
-  (( count++ ))
-  done
-}
-
-function create_edge_hosts_using_mgmt_vcenter {
-  count=1
-  echo "[nsxedges]" > edge_vms
-  for edge_ip in $(echo $NSX_T_EDGE_IPS | sed -e 's/,/ /g')
-  do
-    cat >> edge_vms <<-EOF
-${NSX_T_EDGE_HOST_PREFIX}-0${count}  \
-  ansible_ssh_host=$edge_ip \
-  ansible_ssh_user=root \
-  ansible_ssh_pass=$NSX_T_EDGE_ROOT_PWD \
-  vcenter_host="$VCENTER_HOST" \
-  vcenter_user="$VCENTER_USR" \
-  vcenter_pwd="$VCENTER_PWD" \
-  dc="$VCENTER_DATACENTER" \
-  datastore="$VCENTER_DATASTORE" \
-  cluster="$VCENTER_CLUSTER" \
-  resource_pool="$VCENTER_RP" \
-  dns_server="$DNSSERVER" \
-  dns_domain="$DNSDOMAIN" \
-  ntp_server="$NTPSERVERS" \
-  gw=$DEFAULTGATEWAY \
-  mask=$NETMASK \
-  vmname="${NSX_T_EDGE_VM_NAME_PREFIX}-0${count}" \
-  hostname="${NSX_T_EDGE_HOST_PREFIX}-0${count}" \
-  portgroup="$MGMT_PORTGROUP" \
-  portgroupExt="$NSX_T_EDGE_PORTGROUP_EXT" \
-  portgroupTransport="$NSX_T_EDGE_PORTGROUP_TRANSPORT"
+edge-${count} ip=$edge_ip hostname=${edge_hostname_prefix_int}-${count} default_gateway=$edge_default_gateway_int prefix_length=$edge_ip_prefix_length_int edge_fabric_node_name=${edge_fabric_name_prefix_int}-${count}  transport_node_name=${edge_transport_node_prefix_int}-${count}
+# ${NSX_T_EDGE_HOST_PREFIX}-0${count}  \
+#   ansible_ssh_host=$edge_ip \
+#   ansible_ssh_user=root \
+#   ansible_ssh_pass=$NSX_T_EDGE_ROOT_PWD \
+#   vcenter_host="$VCENTER_HOST" \
+#   vcenter_user="$VCENTER_USR" \
+#   vcenter_pwd="$VCENTER_PWD" \
+#   dc="$VCENTER_DATACENTER" \
+#   datastore="$VCENTER_DATASTORE" \
+#   cluster="$VCENTER_CLUSTER" \
+#   resource_pool="$VCENTER_RP" \
+#   dns_server="$DNSSERVER" \
+#   dns_domain="$DNSDOMAIN" \
+#   ntp_server="$NTPSERVERS" \
+#   gw=$DEFAULTGATEWAY \
+#   mask=$NETMASK \
+#   vmname="${NSX_T_EDGE_VM_NAME_PREFIX}-0${count}" \
+#   hostname="${NSX_T_EDGE_HOST_PREFIX}-0${count}" \
+#   portgroup="$MGMT_PORTGROUP" \
+#   portgroupExt="$NSX_T_EDGE_PORTGROUP_EXT" \
+#   portgroupTransport="$NSX_T_EDGE_PORTGROUP_TRANSPORT"
 EOF
     (( count++ ))
   done
+
+  cat >> edge_vms <<-EOF
+[edge_nodes:vars]
+edge_cli_password="$edge_cli_password_int"
+edge_root_password="$edge_root_password_int"
+edge_deployment_size="$edge_deployment_size_int"
+edge_compute_id="$edge_compute_id_int"
+edge_storage_id="$edge_storage_id_int"
+edge_data_network_id="$edge_data_network_id_int"
+edge_management_network_id="$edge_management_network_id_int"
+EOF
 }
 
+### TODO: where are transport node config params
 function create_esxi_hosts {
   echo "$ESXI_HOSTS_CONFIG" > /tmp/esxi_hosts_config.yml
   touch esxi_hosts
@@ -189,102 +100,54 @@ EOF
   fi
 }
 
+## TODO: convert = notation to : notation when specifiying variables
 function create_hosts {
 
+# TODO: set nsx manager fqdn
 export NSX_T_MANAGER_SHORT_HOSTNAME=$(echo $NSX_T_MANAGER_FQDN | awk -F '\.' '{print $1}')
 
 cat > hosts <<-EOF
 [localhost]
 localhost       ansible_connection=local
 
-[nsxmanagers]
-nsx-manager  \
-  ansible_ssh_host=$NSX_T_MANAGER_IP \
-  ansible_ssh_user=root \
-  ansible_ssh_pass=$NSX_T_MANAGER_ROOT_PWD \
-  dc="$VCENTER_DATACENTER" \
-  cluster="$VCENTER_CLUSTER" \
-  resource_pool="$VCENTER_RP" \
-  datastore="$VCENTER_DATASTORE" \
-  portgroup="$MGMT_PORTGROUP" \
-  gw=$DEFAULTGATEWAY \
-  mask=$NETMASK \
-  vmname="$NSX_T_MANAGER_VM_NAME" \
-  hostname="$NSX_T_MANAGER_SHORT_HOSTNAME"
-
 [localhost:vars]
-
-ovfToolPath='/usr/bin'
-nsxOvaPath="$OVA_ISO_PATH"
 sshEnabled='True'
 allowSSHRootAccess='True'
-managerOva=$NSX_T_MANAGER_OVA
-controllerOva=$NSX_T_CONTROLLER_OVA
-edgeOva=$NSX_T_EDGE_OVA
 
-deployVcIPAddress="$VCENTER_HOST"
-deployVcUser=$VCENTER_USR
-deployVcPassword="$VCENTER_PWD"
-compute_manager="$VCENTER_MANAGER"
-cm_cluster="$VCENTER_CLUSTER"
+vcenter_ip="$vcenter_ip_int"
+vcenter_username="$vcenter_username_int"
+vcenter_password="$vcenter_password_int"
+vcenter_folder="$vcenter_folder_int"
+vcenter_datacenter="$vcenter_datacenter_int"
+vcenter_cluster="$vcenter_cluster_int"
+vcenter_datastore="$vcenter_datastore_int"
 
-compute_vcenter_host="$COMPUTE_VCENTER_HOST"
-compute_vcenter_user="$COMPUTE_VCENTER_USR"
-compute_vcenter_password="$COMPUTE_VCENTER_PWD"
-compute_vcenter_cluster="$COMPUTE_VCENTER_CLUSTER"
-compute_vcenter_manager="$COMPUTE_VCENTER_MANAGER"
+mgmt_portgroup="$mgmt_portgroup_int"
+dns_server="$dns_server_int"
+dns_domain="$dns_domain_int"
+ntp_servers="$ntp_servers_int"
+default_gateway="$default_gateway_int"
+netmask="$netmask_int"
+path_to_ova="$path_to_ova_int"
+ova_file_name="$ova_file_name_int"
+ovftool_image="$ovftool_image_int"
 
-edge_vcenter_host="$EDGE_VCENTER_HOST"
-edge_vcenter_user="$EDGE_VCENTER_USR"
-edge_vcenter_password="$EDGE_VCENTER_PWD"
-edge_vcenter_cluster="$EDGE_VCENTER_CLUSTER"
-edge_dc="$EDGE_VCENTER_DATACENTER"
-edge_datastore="$EDGE_VCENTER_DATASTORE"
-edge_portgroup="$EDGE_MGMT_PORTGROUP"
-edge_dns_server="$EDGE_DNSSERVER"
-edge_dns_domain="$EDGE_DNSDOMAIN"
-edge_ntp_server="$EDGE_NTPSERVERS"
-edge_gw="$EDGE_DEFAULTGATEWAY"
-edge_mask="$EDGE_NETMASK"
+nsx_manager_ip="$nsx_manager_ip_int"
+nsx_manager_username="$nsx_manager_username_int"
+nsx_manager_password="$nsx_manager_password_int"
+nsx_manager_assigned_hostname="$nsx_manager_assigned_hostname_int"
+nsx_manager_root_pwd="$nsx_manager_root_pwd_int"
+nsx_manager_deploy_size="$nsx_manager_deploy_size_int"
 
-nsxInstaller="$NSX_T_INSTALLER"
-nsxAdminPass="$NSX_T_MANAGER_ADMIN_PWD"
-nsxCliPass="$NSX_T_MANAGER_ROOT_PWD"
-
-dns_server="$DNSSERVER"
-dns_domain="$DNSDOMAIN"
-ntp_server="$NTPSERVERS"
-
-# Sizing of vms for deployment
-nsx_t_mgr_deploy_size="$NSX_T_MGR_DEPLOY_SIZE"
-nsx_t_edge_deploy_size="$NSX_T_EDGE_DEPLOY_SIZE"
-
-tag_scope="ncp/cluster"
-tag=$NSX_T_PAS_NCP_CLUSTER_TAG
-overlay_tz_name=$NSX_T_OVERLAY_TRANSPORT_ZONE
-vlan_tz_name=$NSX_T_VLAN_TRANSPORT_ZONE
-vlan_hostswitch=$NSX_T_VLAN_HOSTSWITCH
-overlay_hostswitch=$NSX_T_OVERLAY_HOSTSWITCH
-
-tep_pool_name=$NSX_T_TEP_POOL_NAME
-tep_pool_cidr=$NSX_T_TEP_POOL_CIDR
-tep_pool_range="${NSX_T_TEP_POOL_START}-${NSX_T_TEP_POOL_END}"
-tep_pool_gw=$NSX_T_TEP_POOL_GATEWAY
-
-edge_single_uplink_profile_name=$NSX_T_SINGLE_UPLINK_PROFILE_NAME
-edge_single_uplink_profile_mtu=$NSX_T_SINGLE_UPLINK_PROFILE_MTU
-edge_single_uplink_profile_vlan=$NSX_T_SINGLE_UPLINK_PROFILE_VLAN
-edge_interface=$NSX_T_EDGE_OVERLAY_INTERFACE
-edge_uplink_interface=$NSX_T_EDGE_UPLINK_INTERFACE
-
-esxi_uplink_vmnics_arr="${esxi_host_uplink_vmnics}"
-edge_uplink_vmnics_arr="${edge_host_uplink_vmnics}"
-
-esxi_overlay_profile_name=$NSX_T_OVERLAY_PROFILE_NAME
-esxi_overlay_profile_mtu=$NSX_T_OVERLAY_PROFILE_MTU
-esxi_overlay_profile_vlan=$NSX_T_OVERLAY_PROFILE_VLAN
-
-edge_cluster="$NSX_T_EDGE_CLUSTER"
+compute_manager_username="$compute_manager_username_int"
+compute_manager_password="$compute_manager_password_int"
+edge_uplink_profile_vlan="$edge_uplink_profile_vlan_int"
+esxi_uplink_profile_vlan="$esxi_uplink_profile_vlan_int"
+vtep_ip_pool_name="$vtep_ip_pool_name_int"
+vtep_ip_pool_cidr="$vtep_ip_pool_cidr_int"
+vtep_ip_pool_gateway="$vtep_ip_pool_gateway_int"
+vtep_ip_pool_start="$vtep_ip_pool_start_int"
+vtep_ip_pool_end="$vtep_ip_pool_end_int"
 
 EOF
 
